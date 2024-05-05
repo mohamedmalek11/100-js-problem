7e0920a799dad5d365cc6079fe7b146d8f3b8aa5
MohamedMalek <mobile@matjrah.com>
2024-05-05 08:22:33 +0300

    Added 11 A Needle in the Haystack

--- 0_convert_name_to_file_ext.js
+++ 0_convert_name_to_file_ext.js
@@ -4,5 +4,5 @@ function convertNameToFileName(num,text) {
   return num + "_" + text.toLowerCase().split(" ").join("_") + ".js"
 }
 console.log(
-  convertNameToFileName(10 , "A Needle in the Haystack")
+  convertNameToFileName(11 , "A Needle in the Haystack")
 );
---
+++ 11_count_of_positives_sum_of_negatives.js
@@ -0,0 +1,22 @@
+// Given an array of integers.
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+// If the input is an empty array or is null, return an empty array.
+
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+function countPositivesSumNegatives(input) {
+  let negative = 0;
+  let positive = 0;
+  if (!input || input == 0) return;
+  input.map((i) => {
+    return i > 0 ? positive++ : (negative += i);
+  });
+  return [positive, negative];
+}
+
+console.log(
+  countPositivesSumNegatives([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15,
+  ])
+);
