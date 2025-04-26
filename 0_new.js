d9e45e41eb59f927c8c5f4971ad1dc850d7328ba
mohamedmalek11 <mohamed.malek.msm@gmail.com>
2025-04-26 21:20:36 +0300

    Added 32

--- 0_new.js
+++ 0_new.js
@@ -1,13 +1,15 @@
 import fs from "fs";
 import path from "path";
 
+const number = "32";
+const name = "Transportation On Vacation";
+
 // convert challenge name to file name for automate naming
 function convertNameToFileName(num, text) {
   return num + "_" + text.toLowerCase().split(" ").join("_") + ".js";
 }
 
-const outputPath = path.join(convertNameToFileName("2222", "my own problem"));
+const outputPath = path.join(convertNameToFileName(number, name));
 fs.writeFileSync(outputPath, "");
 
 console.log("✅ New problem created & saved to:", outputPath);
-
